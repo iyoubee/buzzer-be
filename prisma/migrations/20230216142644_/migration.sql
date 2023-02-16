9deabf9bfea73354da918adf690e78f35d91ef59
iyoubee <al.ayubi2020@gmail.com>
2023-02-16 22:07:42 +0700

    final

--- prisma/migrations/20230216142644_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `hasedRt` on the `users` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "users" DROP COLUMN "hasedRt",
-ADD COLUMN     "hashedRt" TEXT;
