336d5ff883aa75544525e4ed5a588d05a9886c59
iyoubee <al.ayubi2020@gmail.com>
2023-02-24 21:55:38 +0700

    idk

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { UserService } from './user.service';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Get,
+  UseGuards,
+  Param,
+} from '@nestjs/common';
+import { Message, User } from '@prisma/client';
+
+import { Public, GetCurrentUserId, GetCurrentUser } from '../common/decorators';
+import { RtGuard, AtGuard } from '../common/guards';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Public()
+  @Get('getMessage/:username')
+  @HttpCode(HttpStatus.OK)
+  getUser(
+    @GetCurrentUserId() userId: number,
+    @Param('username') username: string,
+  ): Promise<Message[] | undefined> {
+    return this.userService.getMessage(userId, username);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,52 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Message } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService, private config: ConfigService) {}
+
+  async getMessage(
+    userId: number | undefined,
+    username: string,
+  ): Promise<Message[] | undefined> {
+    const userIdDB = await this.prisma.user.findUnique({
+      where: { username: username },
+      select: {
+        id: true,
+      },
+    });
+    if (userId) {
+      if (userId == userIdDB?.id) {
+        const message = await this.prisma.message.findMany({
+          where: {
+            author: {
+              username: username,
+            },
+          },
+        });
+        return message;
+      } else {
+        const message = await this.prisma.message.findMany({
+          where: {
+            author: {
+              username: username,
+            },
+          },
+        });
+        message.filter((message) => message.closeFriends.includes(userId));
+        return message;
+      }
+    }
+    const message = await this.prisma.message.findMany({
+      where: {
+        author: {
+          username: username,
+        },
+        isCloseFriends: false,
+      },
+    });
+    return message;
+  }
+}
