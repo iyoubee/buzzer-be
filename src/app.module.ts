315dea7be97ea230e11224ebedd1938898563535
iyoubee <al.ayubi2020@gmail.com>
2023-02-24 16:01:58 +0700

    make some module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,16 @@ import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { AtGuard } from './common/guards';
+import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
+    PrismaModule,
+  ],
   providers: [
     {
       provide: APP_GUARD,
