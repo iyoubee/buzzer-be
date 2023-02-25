347a8fd3c4949880f88dfb0655bacd689e9a4e4d
iyoubee <al.ayubi2020@gmail.com>
2023-02-25 19:22:49 +0700

    save

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -70,6 +70,9 @@ export class UserService {
         where: {
           author: { username: username },
         },
+        include: {
+          author: true,
+        },
       });
       return message;
     }
