b3925641a46d534dd717f3f85b5ec77c950f0977
iyoubee <al.ayubi2020@gmail.com>
2023-02-26 05:37:30 +0700

    try #3

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     allowedHeaders: ['content-type'],
-    origin: 'https://buzzer-be.vercel.app',
+    origin: 'https://buzzer-be.vercel.app/',
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
